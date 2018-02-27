c0e6ff35996c24e52bc16294ddad9a75b8ee6016
Yauheni Dubouski <joinhud@gmail.com>
2018-02-27 21:52:09 +0300

    advanced-zeros/feature: implement function which calulate how many zeros in the end of number, which is factorial of number in base base system

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let result = 0;
+  let multipliers = calcSimpleMultipliers(base);
+  let degree = multipliers.length;
+  base = multipliers[0];
+
+  while (number > 0) {
+    number = Math.floor(number / base);
+    result += number;
+  }
+
+  return Math.floor(result / degree);
+}
+
+function calcSimpleMultipliers(x) {
+  const minMultiplier = 2;
+  let i = minMultiplier;
+  let max = minMultiplier;
+  let multipliers = [];
+  let result = [];
+
+  while (i <= x) {
+    if (x % i === 0) {
+      if (i > max) {
+        max = i;
+      }
+
+      multipliers.push(i);
+      x /= i;
+    }
+    else {
+      i++;
+    }
+  }
+
+  let maxMultipliersArray = multipliers.filter(element => element === max);
+  let minMultipliersArray = multipliers.filter(element => element === minMultiplier);
+
+
+  if (minMultipliersArray.length >= Math.pow(max, maxMultipliersArray.length)) {
+    result = minMultipliersArray;
+  } else {
+    result = maxMultipliersArray;
+  }
+
+  return result;
 }
